1c90c072784be700eec8c24952564e6a6e410d40
SuGit-Demo <89434151+SuGit-Demo@users.noreply.github.com>
2022-06-30 14:18:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, jsonify, render_template
 import numpy as np
-import pickle
+#import pickle
+import requests
 
 app = Flask(__name__)
-
-model = pickle.load(open('model.h5', 'rb'))
 
 @app.route('/')
 def home():
@@ -12,14 +11,78 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
+    LoanDuration = request.form.get('duration')
+    LoanAmount = request.form.get('amount')
+    Age = request.form.get('age')
+        
+    #output = round(prediction[0], 2)    
+   
+    ####################### FROM AUTOAI DEPLOYMENT API #######################
+    # NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
+    API_KEY = "JFEp-T-YU4Wsdf5bxxTILX1avq0HTCdO0t4P_EMt-RNm"
+    token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":
+     API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+    mltoken = token_response.json()["access_token"]
 
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
 
-    output = round(prediction[0], 2)
+    #LoanDuration = input('Enter Loan Duration')
+    #LoanAmount = input('Enter Loan Amount')
+    #Age = input('Enter Age')
 
-    return render_template('index.html', prediction_text='Sales should be $ {}'.format(output))
+    # NOTE: manually define and pass the array(s) of values to be scored in the next line
+    payload_scoring = {"input_data": [{"fields": [
+                    "CheckingStatus",
+                    "LoanDuration",
+                    "CreditHistory",
+                    "LoanPurpose",
+                    "LoanAmount",
+                    "ExistingSavings",
+                    "EmploymentDuration",
+                    "InstallmentPercent",
+                    "Sex",
+                    "OthersOnLoan",
+                    "CurrentResidenceDuration",
+                    "OwnsProperty",
+                    "Age",
+                    "InstallmentPlans",
+                    "Housing",
+                    "ExistingCreditsCount",
+                    "Job",
+                    "Dependents",
+                    "Telephone",
+                    "ForeignWorker"],
+        "values": [[
+
+                    None,
+                    LoanDuration,
+                    None,
+                    None,
+                    LoanAmount,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    Age,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None ]]
+        }]}
+
+    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/7a871bb8-b73b-4099-b4b5-34ff8a331883/predictions?version=2022-06-30', json=payload_scoring,
+     headers={'Authorization': 'Bearer ' + mltoken})
+    print("Scoring response")
+    print(response_scoring.json())
+    ####################### END OF AUTOAI DEPLOYMENT API #######################
+
+    return render_template('index.html', prediction_text='Sales should be $ {}'.format(response_scoring.json()))
 
 @app.route('/results',methods=['POST'])
 def results():
